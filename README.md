7da86d009f8cb2a50c22fded8d053a71816a351e
Aleksa Stamenkovic <astamenkovic@ucsd.edu>
2022-11-18 19:20:57 -0800

    finished lab without part 2 questions

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Lab 8 - Starter
+
+1. Manually run them locally before pushing code -> because the code is realtively small with not many fetures, also we want pushed code to already be tested as a team partner has to continue work and be sure that the code base is true
+2. no
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -1,5 +1,24 @@
 // unit.test.js
 
-const functions = require('../code-to-unit-test/unit-test-me.js');
+const functions = require("../code-to-unit-test/unit-test-me.js");
 
 // TODO - Part 2
+test("is 111-111-1111 a phone number", () => {
+  expect(functions.isPhoneNumber('111-111-1111')).toBe(true);
+});
+
+test("is test_address@ucsd.edu a phone number", () => {
+  expect(functions.isEmail("test_address@ucsd.edu")).toBe(true);
+});
+
+test("is A23Rfs_4 a strong password", () => {
+  expect(functions.isStrongPassword("A23Rfs_4")).toBe(true);
+});
+
+test("is 01/01/2001 a date", () => {
+  expect(functions.isDate("01/01/2001")).toBe(true);
+});
+
+test("is #ffffff a hex color", () => {
+  expect(functions.isHexColor("#abc123")).toBe(true);
+});
